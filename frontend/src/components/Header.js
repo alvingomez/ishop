4bca7f4221f943daf538930e71f4f373dbfb3242
Alvin Gomez <alvingomez.world@gmail.com>
2021-01-09 23:18:31 +0800

    Car and Sign-in links
    I placed the container inside Navbar and place the links to the right hand side of the page.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,15 +4,17 @@ import {Navbar, Nav, Container} from 'react-bootstrap';
 const Header = () => {
     return <header>
     <Navbar bg="dark" variant='dark' expand="lg" CollapseOnSelect>
-    <Navbar.Brand href="/">iShop</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
+    <Container>
+     <Navbar.Brand href="/">iShop</Navbar.Brand>
+     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+     <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="ml-auto">
       <Nav.Link href="/cart">Cart</Nav.Link>
       <Nav.Link href="/login">Sign In</Nav.Link>      
-    </Nav>    
-  </Navbar.Collapse>
-</Navbar>           
+      </Nav>    
+     </Navbar.Collapse>
+    </Container>
+    </Navbar>           
     </header>
 }
 
