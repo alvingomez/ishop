d1250122411bc222393c30094a77c068e9fd54bb
Alvin Gomez <alvingomez.world@gmail.com>
2021-01-13 23:13:39 +0800

    Card Text
    Product rating and product reviews added along with the product price

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -10,14 +10,21 @@ const Product = ({product}) => {
                 <Card.Img src={product.image} variant='top' />
             </a>
 
-        <Card.Body>
+         <Card.Body>
             <a href={`/product/${product._id}`}>
                 <Card.Title as='div'>
                     <strong>{product.name}</strong>
                 </Card.Title>
             </a>
-        </Card.Body>
 
+             <Card.Text as='div'>
+                <div className="my-3">
+                    {product.rating} from {product.numReviews} reviews
+                </div>
+             </Card.Text>
+             
+             <Card.Text as='h3'>${product.price}</Card.Text>
+         </Card.Body>        
         </Card>
     )
 }
