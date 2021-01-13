15c97c41e3d67c27d396020331cb4d548d4ef3ae
Alvin Gomez <alvingomez.world@gmail.com>
2021-01-13 22:43:25 +0800

    Card Title
    Card title added

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -3,13 +3,24 @@ import {Card} from 'react-bootstrap';
 
 //Using product as a prop instead of props so as not to use prop.product
 const Product = ({product}) => {
+    
     return (
         <Card className='my-3 p-3 rounded'>
             <a href={`/product/${product._id}`}>
                 <Card.Img src={product.image} variant='top' />
             </a>
+
+        <Card.Body>
+            <a href={`/product/${product._id}`}>
+                <Card.Title as='div'>
+                    <strong>{product.name}</strong>
+                </Card.Title>
+            </a>
+        </Card.Body>
+
         </Card>
     )
 }
+
 
 export default Product
